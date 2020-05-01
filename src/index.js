16405b68c02ce73682720572426120cba0045ca9
wpho <u.pho@tcu.edu>
2020-05-01 17:41:56 -0500

    Add files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import './style.css';
+import Icon from'./icon.jpg';
+import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
   
     return element;
   }
